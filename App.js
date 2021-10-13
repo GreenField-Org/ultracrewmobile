ec3807674a816be92ced063cc969b1b45ce42839
Hanna <hannamlittlefield@gmail.com>
2021-10-13 16:33:55 -0400

    ultracrewmobile#24 - re adding package

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { View } from 'react-native'
 import PhoneNumber from './Components/FormComponents/PhoneNumber';
 import LoginScreen from './Components/LoginScreen';
-
+import { NavigationContainer } from '@react-navigation/native'
 export default function App() {
   return(
     <View>
+    <NavigationContainer>
       <LoginScreen/>
       <PhoneNumber/>
+    </NavigationContainer>
     </View>
   )};
