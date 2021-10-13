bceb51be318c0211ac8268a415d7d96df2be7f08
Hanna <hannamlittlefield@gmail.com>
2021-10-13 10:56:44 -0400

    ultracrewmobile#18 - styling phone component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native'
+import PhoneNumber from './Components/FormComponents/PhoneNumber';
 import LoginScreen from './Components/LoginScreen';
 
 export default function App() {
   return(
     <View>
       <LoginScreen/>
+      <PhoneNumber/>
     </View>
   )};
---
+++ Components/FormComponents/PhoneNumber.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import PhoneInput from "react-native-phone-number-input";
+
+export default function PhoneNumber(){
+  const [value, setValue] = useState("");
+  const [valid, setValid] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  //const phoneInput = useRef<PhoneInput>(null);
+  return (
+        <PhoneInput
+            //ref={phoneInput}
+            style={styles.phone}
+            defaultValue={value}
+            defaultCode="IN"
+            onChangeFormattedText={(text) => {
+              setValue(text);
+            }}
+            withDarkTheme
+            withShadow
+            autoFocus
+          />
+  );
+};
+
+const styles = StyleSheet.create({
+    phone: {
+        width: 300,
+    }
+})
