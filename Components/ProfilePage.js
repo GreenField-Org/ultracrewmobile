6a7b023ea0b38d36a8f550a928f8ec2b44e8c235
Hanna <hannamlittlefield@gmail.com>
2021-10-14 12:50:18 -0400

    ultracrewmobile#30 - creating profile page file

---
+++ Components/ProfilePage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { StyleSheet, Button, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+export default function ProfilePage() {
+    return (
+<Text>Profile page will go here.</Text>
+    )
+}
