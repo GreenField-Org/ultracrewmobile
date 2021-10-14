57f9cbf4f88e4f92372495744b9b7c8f03584feb
Hanna <hannamlittlefield@gmail.com>
2021-10-14 12:47:25 -0400

    ultracrewmobile#24 - adding navigation container to app js

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { View } from 'react-native'
 import PhoneNumber from './Components/FormComponents/PhoneNumber';
 import LoginScreen from './Components/LoginScreen';
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
+import CreateAcct from './Components/CreateAcct';
 export default function App() {
   return(
     <View>
     <NavigationContainer>
+    <CreateAcct/>
       <LoginScreen/>
       <PhoneNumber/>
     </NavigationContainer>
