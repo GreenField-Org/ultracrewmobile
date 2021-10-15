d4ec09ab3614399c04f45a5931a753825daa2674
Daniel Vigil <daniel.julian.vigil@gmail.com>
2021-10-15 15:01:59 -0400

    ultracrewmobile#50 making a default button

---
+++ Components/DefaultButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function DefaultButton ({ buttonText }) {
+    return (
+        <View>
+            <TouchableOpacity style={styles.loginButton}>
+                <Text>{buttonText}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    loginButton: {
+        width:"80%",
+        borderRadius:10,
+        height:50,
+        fontSize: 20,
+        alignItems:"center",
+        justifyContent:"center",
+        backgroundColor:"orange",
+    }
+})
