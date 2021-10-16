47baa808734c112af6a91a18b871c880942643fd
Daniel Vigil <daniel.julian.vigil@gmail.com>
2021-10-16 11:16:57 -0400

    ultracrewmobile#33 creating the aid station view default

---
+++ Views/Aid-Station.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheets, View } from 'react-native';
+
+export default function AidStation ({ stationName }) {
+    return (
+    <View>
+        <Text>Aid Station {stationName} </Text>
+    </View>
+    )
+}
