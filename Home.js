1edab92c3863c080c43cd6415d4261864fffa6ad
Hanna <hannamlittlefield@gmail.com>
2021-10-25 13:18:29 -0400

    ultracrewmobile#24 - working on nav

--- Home.js
+++ Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import DefaultButton from './Components/DefaultButton';
+import { Button } from 'react-native';
 
 
 /*  This should be moved into the View folder once all page creations are merged back into main */ 
@@ -9,6 +10,22 @@ export default function Home({ navigation }) {
     return (
         <View>
             <Text>Main Menu</Text>
+            <Button
+            title="My Races"
+            onPress={() => navigation.navigate('MyRaces')}
+          />
+          <Button
+          title="Profile"
+          onPress={() => navigation.navigate('Profile')}
+        />
+        <Button
+        title="Race"
+        onPress={() => navigation.navigate('Race')}
+      />
+      <Button
+      title="Create Aid Station"
+      onPress={() => navigation.navigate('AidStation')}
+    />
         </View>
     )
 }
