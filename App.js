78d3749a784b1d7d06a23027553be45ab80601d4
Daniel Vigil <daniel.julian.vigil@gmail.com>
2021-10-25 20:11:04 -0400

    #56 application title font error correction

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View } from 'react-native'
-import PhoneNumber from './Components/FormComponents/PhoneNumber';
 import LoginScreen from './Components/LoginScreen';
 
 export default function App() {
   return(
-    <View>
+    <View style={{marginTop: 300}}>
       <LoginScreen/>
-      <PhoneNumber/>
     </View>
   )};
