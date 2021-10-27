6f2b22ffc46181b55a5bbf81a6e3de8cc581259c
Hanna <hannamlittlefield@gmail.com>
2021-10-27 14:14:42 -0400

    ultracrewmobile#35 working on home button component -

--- Components/HomeButton.js
+++ Components/HomeButton.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Button } from 'react-native';
-
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function HomeButton({ navigation }) {
     return (
+        <View>
         <Button
         title="Home"
+        style={styles.homebutton}
         onPress={() => navigation.navigate('Home')}
         />
+        </View>
     )
     }
+
+    const styles = StyleSheet.create({
+        homebutton: {
+            borderRadius: 100,
+        }
+    })
