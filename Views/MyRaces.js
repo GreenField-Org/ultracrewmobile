9c7f19e25c1ae4d0b14d3d20521a68347e2bf744
Hanna <hannamlittlefield@gmail.com>
2021-11-10 10:56:17 -0500

    adding button with nav feature to My Races page

--- Views/MyRaces.js
+++ Views/MyRaces.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import DefaultButton from '../Components/DefaultButton';
 
 /* This should be moved into the View folder once all pages are merged into main */
 
-export default function MyRaces() {
+export default function MyRaces({navigation}) {
     return (
         <View>
-            <Text>My Races</Text>
+            <DefaultButton 
+                buttonText='Create A New Race'
+                onPress={() => navigation.navigate('MyRaces')}/>
+            <DefaultButton 
+                buttonText='Current Races'
+                onPress={() => navigation.navigate('CurrentRaces')}/>
+            <DefaultButton 
+                buttonText='Completed Races'
+                onPress={() => navigation.navigate('CompletedRaces')}/>
         </View>
     )
 }
