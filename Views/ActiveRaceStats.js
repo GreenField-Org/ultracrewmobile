b9c4e1d6c4b5e177d88e64f8e272c7b03192a76f
Hanna <hannamlittlefield@gmail.com>
2022-08-02 20:10:32 -0400

    adding dummy page for active race stats

---
+++ Views/ActiveRaceStats.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+
+const customFont = {
+  "Lobster-Regular": require("../assets/fonts/Lobster-Regular.ttf"),
+};
+
+export default function CompletedRaceStats() {
+    let [fontsLoaded] = useFonts(customFont);
+
+    if (fontsLoaded) {
+        return (
+            <View>
+                <Text>Active race stats will go here</Text>
+            </View>
+        )   
+    } else {
+        return <View/>
+    }
+}
+
+const styles = StyleSheet.create({
+    titleText: {
+        fontSize: 30,
+        marginBottom: 50,
+        fontFamily: 'Lobster-Regular',
+    },
+})
