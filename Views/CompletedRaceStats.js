5671bb4d5c64cb38b18b0ae7e39b9e4fb6e240eb
Hanna <hannamlittlefield@gmail.com>
2022-08-04 15:20:36 -0400

    adding dummy data to table

--- Views/CompletedRaceStats.js
+++ Views/CompletedRaceStats.js
@@ -7,13 +7,41 @@ const customFont = {
   "Lobster-Regular": require("../assets/fonts/Lobster-Regular.ttf"),
 };
 
+const completedRace = {
+    name: "Cocodona",
+    date: "August 4, 2020",
+    totalTime: "4:44:44",
+    averagePace: "12:34/mile",
+    distanceRan: 250,
+    calConsume: 1234,
+    calBurned: 5678
+}
+
 export default function CompletedRaceStats() {
     let [fontsLoaded] = useFonts(customFont);
 
     if (fontsLoaded) {
         return (
-            <View>
-                <Text>Completed race stats will go here</Text>
+            <View style={styles.container}>
+                <Text style={styles.titleText}>{completedRace.name}</Text>
+                <Grid>
+                    <Col>
+                        <Row><Text>Date</Text></Row>
+                        <Row><Text>Total Time</Text></Row>
+                        <Row><Text>Average Pace</Text></Row>
+                        <Row><Text>Distance Run</Text></Row>
+                        <Row><Text>Calories Consumed</Text></Row>
+                        <Row><Text>Calories Burned</Text></Row>
+                    </Col>
+                    <Col>
+                        <Row><Text>{completedRace.date}</Text></Row>
+                        <Row><Text>{completedRace.totalTime}</Text></Row>
+                        <Row><Text>{completedRace.averagePace}</Text></Row>
+                        <Row><Text>{completedRace.distanceRan} miles</Text></Row>
+                        <Row><Text>{completedRace.calConsume} calories</Text></Row>
+                        <Row><Text>{completedRace.calBurned} calories</Text></Row>
+                    </Col>
+</Grid>
             </View>
         )   
     } else {
@@ -26,5 +54,11 @@ const styles = StyleSheet.create({
         fontSize: 30,
         marginBottom: 50,
         fontFamily: 'Lobster-Regular',
+    },
+    container: {
+        backgroundColor: '#F8CD82',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
     },
 })
