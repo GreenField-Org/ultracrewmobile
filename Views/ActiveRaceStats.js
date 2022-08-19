fc0430df08dd1934f01efeb11dd2b6822b877448
Hanna <hannamlittlefield@gmail.com>
2022-08-19 12:48:22 -0400

    adding Log Aid Station button to active race stats page

--- Views/ActiveRaceStats.js
+++ Views/ActiveRaceStats.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import { useFonts } from "expo-font";
 
 const customFont = {
   "Lobster-Regular": require("../assets/fonts/Lobster-Regular.ttf"),
 };
 
-export default function ActiveRaceStats() {
+export default function ActiveRaceStats({ navigation }) {
     let [fontsLoaded] = useFonts(customFont);
 
     if (fontsLoaded) {
         return (
             <View>
                 <Text>Active race stats will go here</Text>
+                <Pressable
+                title="Login"
+                style={styles.createAidButton}
+                onPress={() => navigation.navigate("CreateAid")}
+              >
+                <Text style={styles.text}>Log Aid Station</Text>
+              </Pressable>
             </View>
         )   
     } else {
@@ -25,5 +32,16 @@ const styles = StyleSheet.create({
         fontSize: 30,
         marginBottom: 50,
         fontFamily: 'Lobster-Regular',
+    },
+    createAidButton: {
+        width: "80%",
+        borderRadius: 10,
+        marginBottom: 35,
+        height: 50,
+        fontSize: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#0DF7DB",
+        fontWeight: 700,
     },
 })
