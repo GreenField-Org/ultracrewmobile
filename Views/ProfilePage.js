0ea8b25488ecba8a81b9c4e1ee7e7c01a814af39
Hanna <hannamlittlefield@gmail.com>
2022-08-29 14:12:57 -0400

    adding dummy data

--- Views/ProfilePage.js
+++ Views/ProfilePage.js
@@ -6,13 +6,24 @@ const customFont = {
   "Lobster-Regular": require("../assets/fonts/Lobster-Regular.ttf"),
 };
 
+const profileInfo = [{
+    name: 'Hanna Littlefield'},
+    {age: 31},
+    {location: 'Taunton, MA'},
+    {weight: 138},
+    {email: 'hannamlittlefield@gmail.com'},
+    {telephoneNumber: '443-454-18613'},
+    {userTier: 3}
+]
+
 export default function ProfilePage({navigation}) {
     let [fontsLoaded] = useFonts(customFont);
 
     if (fontsLoaded) {
         return (
-            <View>
-                <Text>Profile page will go here.</Text>
+            <View style={styles.container}>
+                <Text style={styles.titleText}>Name</Text>
+                
             </View>
         )   
     } else {
@@ -25,5 +36,11 @@ const styles = StyleSheet.create({
         fontSize: 30,
         marginBottom: 50,
         fontFamily: 'Lobster-Regular',
+    },
+    container: {
+        backgroundColor: '#F8CD82',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
     },
 })
