8ad3695a40b6bc0b9dbe4004f0df44a8108c130c
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-10-30 21:16:52 -0400

    updating styles

--- Views/Home.tsx
+++ Views/Home.tsx
@@ -1,65 +1,67 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Pressable } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import { Pressable } from "react-native";
 
 export default function Home({ navigation }) {
     return (
         <View style={styles.container}>
-          <Text style={styles.text}>Hello, !</Text>
-          <Pressable
-            style={styles.button}
-            onPress={() => navigation.navigate('MyRaces')}>
-            <Text style={styles.text}>View My Races</Text>
-          </Pressable>
-          <Pressable
-          style={styles.button}
-          onPress={() => navigation.navigate('ProfilePage')}>
-            <Text style={styles.text}>My Profile</Text>
-          </Pressable>
-          {/*will need to add functional nagivation*/}
-          <Pressable
-          style={styles.button}
-          onPress={() => navigation.navigate('CreateRace')}>
-            <Text style={styles.text}>Create a Race</Text>
-          </Pressable>
+            <Text style={styles.text}>Hello, !</Text>
+                <Pressable
+                    style={styles.button}
+                    onPress={() => navigation.navigate("MyRaces")}
+                >
+                    <Text style={styles.text}>View My Races</Text>
+                </Pressable>
+                <Pressable
+                    style={styles.button}
+                    onPress={() => navigation.navigate("ProfilePage")}
+                >
+                    <Text style={styles.text}>My Profile</Text>
+                </Pressable>
+                {/*will need to add functional nagivation*/}
+                <Pressable
+                    style={styles.button}
+                    onPress={() => navigation.navigate("CreateRace")}
+                >
+                    <Text style={styles.text}>Create a Race</Text>
+                </Pressable>
 
-          {/*will need to add functional nagivation*/}
-          <Pressable
-            style={styles.button}
-            onPress={() => navigation.navigate()}>
-              <Text style={styles.text}>Settings</Text>
-          </Pressable>
-          <Pressable
-            style={styles.button}
-          >
-            <Text style={styles.text}>Logout</Text>
-          </Pressable>
+                {/*will need to add functional nagivation*/}
+                <Pressable
+                    style={styles.button}
+                    onPress={() => navigation.navigate()}
+                >
+                    <Text style={styles.text}>Settings</Text>
+                </Pressable>
+                <Pressable style={styles.button}>
+                    <Text style={styles.text}>Logout</Text>
+                </Pressable>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-  text:{
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 20,
-    width: "80%",
-    height: 50,
-    paddingHorizontal: 15,
-    borderRadius: 10,
-    elevation: 3,
-    marginTop: 15,
-    backgroundColor: '#0DF7DB',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#F8CD82',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+    text: {
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 20,
+        width: "80%",
+        height: 50,
+        paddingHorizontal: 15,
+        borderRadius: 10,
+        elevation: 3,
+        marginTop: 15,
+        backgroundColor: "#0DF7DB",
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#F8CD82",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
--- Views/LoginScreen.tsx
+++ Views/LoginScreen.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 import { useFonts } from "expo-font";
 
 const customFont = {
-  "Lobster-Regular": require("../assets/fonts/Lobster-Regular.ttf"),
+    "Lobster-Regular": require("../assets/fonts/Lobster-Regular.ttf"),
 };
 
 export default function LoginScreen({ navigation }) {
@@ -14,49 +14,51 @@ export default function LoginScreen({ navigation }) {
 
     if (fontsLoaded) {
         return (
-          <View style={styles.container}>
-            <Text style={styles.titleText}>Ultra Crew App</Text>
-            <View style={styles.loginBox}>
-              <TextInput
-                style={styles.TextInput}
-                placeholder="email"
-                placeholderTextColor="#003f5c"
-                onChangeText={(email) => setUsername(username)}
-                inputMode="email"
-              />
-              <TextInput
-                style={styles.TextInput}
-                placeholder="password"
-                placeholderTextColor="#003f5c"
-                //should we add option to show pw when you're done typing it?
-                secureTextEntry={true}
-                onChangeText={(password) => setPassword(password)}
-              />
+            <View style={styles.container}>
+                <Text style={styles.titleText}>Ultra Crew App</Text>
+                <View style={styles.loginBox}>
+                    <TextInput
+                        style={styles.TextInput}
+                        placeholder="email"
+                        placeholderTextColor="#003f5c"
+                        onChangeText={(email) => setUsername(username)}
+                        inputMode="email"
+                    />
+                    <TextInput
+                        style={styles.TextInput}
+                        placeholder="password"
+                        placeholderTextColor="#003f5c"
+                        //should we add option to show pw when you're done typing it?
+                        secureTextEntry={true}
+                        onChangeText={(password) => setPassword(password)}
+                    />
 
-            <Pressable
-              style={styles.loginButton}
-              onPress={() => navigation.navigate("Home")}
-            >
-              <Text style={styles.text}>Login</Text>
-            </Pressable>
+                    <Pressable
+                        style={styles.loginButton}
+                        onPress={() => navigation.navigate("Home")}
+                    >
+                        <Text style={styles.text}>Login</Text>
+                    </Pressable>
 
-            <Pressable
-              onPress={() => navigation.navigate("CreateAcct")}
-            >
-              <Text style={styles.createAcctBtn}>Create an Account</Text>
-            </Pressable>
+                    <Pressable
+                        onPress={() => navigation.navigate("CreateAcct")}
+                    >
+                        <Text style={styles.createAcctBtn}>
+                            Create an Account
+                        </Text>
+                    </Pressable>
+                </View>
             </View>
-          </View>
-        )
+        );
     } else {
-        return <View />
+        return <View />;
     }
 }
 
 const styles = StyleSheet.create({
     createAcctBtn: {
         fontSize: 16,
-        color: "#F8CD82"
+        color: "#FF9001",
     },
     container: {
         backgroundColor: "#1c1c1c",
@@ -67,30 +69,31 @@ const styles = StyleSheet.create({
     titleText: {
         fontSize: 50,
         marginBottom: 50,
+        paddingTop: 10,
         fontFamily: "Lobster-Regular",
-        color: "#F8CD82"
+        color: "#FF9001",
     },
     loginBox: {
-      alignItems: "center",
-      borderColor: "#ebebeb",
-      borderWidth: 1,
-      boxShadow: "#F8CD82",
-      borderTopLeftRadius: 30,
-      borderTopRightRadius: 30,
-      width: "100%",
-      paddingVertical: 100,
-      paddingHorizontal: 10,
-      marginTop: 50,
-      bottom: -50,
+        alignItems: "center",
+        borderColor: "#ebebeb",
+        borderWidth: 1,
+        boxShadow: "#FF9001",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        width: "100%",
+        paddingVertical: 100,
+        paddingHorizontal: 10,
+        marginTop: 50,
+        bottom: -30,
     },
     text: {
-      fontSize: 20,
-      fontWeight: "600",
+        fontSize: 20,
+        fontWeight: "600",
     },
     TextInput: {
-      backgroundColor: "#EBEBEB",
+        backgroundColor: "#C4C4C4",
         height: 60,
-        width: "100%",
+        width: "80%",
         borderRadius: 50,
         flex: 1,
         fontSize: 20,
@@ -100,10 +103,10 @@ const styles = StyleSheet.create({
     },
     createAcct: {
         marginTop: 60,
-        marginBottom: 60
+        marginBottom: 60,
     },
     loginButton: {
-        width: "100%",
+        width: "40%",
         borderRadius: 50,
         marginBottom: 35,
         marginTop: 10,
