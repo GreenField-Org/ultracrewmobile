0646f3c7c6ef856188973fefc3c37b7ded330e51
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-11-02 21:30:33 -0400

    Refactoring Aid Station component to TS

--- Views/AidStation.tsx
+++ Views/AidStation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheets, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function AidStation ({ stationName }) {
     return (
