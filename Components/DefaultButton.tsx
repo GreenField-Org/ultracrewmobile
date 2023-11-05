28d0d41134ebb3b232054d59fffc68e8c3474520
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-11-04 21:31:52 -0400

    converting default button to TS

--- Components/DefaultButton.tsx
+++ Components/DefaultButton.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, Pressable, View } from 'react-native';
 
-export default function DefaultButton ({ buttonText }) {
+export default function DefaultButton ({ buttonText, navigation }) {
     return (
         <View style={{ width: "100%" }}>
             <Pressable
-                title="Login"
                 style={styles.loginButton}
                 onPress={() => navigation.navigate('Home')}
             >
-                <Text style={styles.text}>Login</Text>
+                <Text style={styles.text}>{buttonText}</Text>
             </Pressable>
         </View>
     )
@@ -25,5 +24,10 @@ const styles = StyleSheet.create({
         alignItems:"center",
         justifyContent:"center",
         backgroundColor:"#0DF7DB",
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
     }
 })
